7dae1c9abf32d2c704bf2c0a3594d72ce0beaa90
PierreSQS <nomo_messina@web.de>
2023-02-05 03:09:31 +0100

    Sec11_Chap126-JPA Updates for MySQL: fixed the SQL-Issues on creating the tables

--- src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
+++ src/main/java/guru/springframework/spring6restmvc/entities/Customer.java
@@ -3,6 +3,8 @@ package guru.springframework.spring6restmvc.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -20,7 +22,10 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    // converting the binary into a string since UUID-Type
+    @JdbcTypeCode(SqlTypes.CHAR)
+    // the length in varchar is needed in MySQL
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     private String name;
 
