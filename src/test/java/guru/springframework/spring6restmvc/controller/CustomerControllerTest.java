3e91f5c1d3e72fef85e25b1f5afbaf733a557657
PierreSQS <nomo_messina@web.de>
2023-02-26 17:43:02 +0100

    Sec7_Assn10: test get customer per Id

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import guru.springframework.spring6restmvc.services.CustomerServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -33,6 +37,20 @@ class CustomerControllerTest {
         mockMvc.perform(get("/api/v1/customer"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(3))
+                .andDo(print());
+    }
+
+    @Test
+    void getCustomerById() throws Exception {
+        List<Customer> allCustomers = customerServImpl.getAllCustomers();
+        Customer customer = allCustomers.get(0);
+
+        System.out.println(allCustomers);
+
+        given(customerMockServ.getCustomerById(any())).willReturn(customer);
+        mockMvc.perform(get("/api/v1/customer/{customerId}",customer.getId()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value(customer.getName()))
                 .andDo(print());
     }
 }
