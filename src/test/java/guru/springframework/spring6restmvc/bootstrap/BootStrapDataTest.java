233e16aa6671d963f3e326ce9b15ee7646c2347f
PierreSQS <nomo_messina@web.de>
2023-09-14 01:36:31 +0200

    Sec9_Assn15: optimized Test

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootStrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootStrapDataTest.java
@@ -2,16 +2,15 @@ package guru.springframework.spring6restmvc.bootstrap;
 
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
+@AutoConfigureDataJpa
 class BootStrapDataTest {
 
     @Autowired
@@ -20,16 +19,8 @@ class BootStrapDataTest {
     @Autowired
     BeerRepository beerRepo;
 
-    BootStrapData bootStrapData;
-
-    @BeforeEach
-    void setUp() {
-        bootStrapData = new BootStrapData(beerRepo,customerRepo);
-    }
-
     @Test
     void testLoadData() {
-        bootStrapData.run("");
         assertThat(beerRepo.count()).isEqualTo(3);
         assertThat(customerRepo.count()).isEqualTo(3);
     }
