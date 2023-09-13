0d3160b0b1e6999cfe865b97d6909bad15ba988e
PierreSQS <nomo_messina@web.de>
2023-09-13 15:42:52 +0200

    Sec9_Chap96-Spring Boot JPA Test Slice-NOK: added repos and JPA Tests

---
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.spring6restmvc.repositories;
+
+import guru.springframework.spring6restmvc.entities.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@DataJpaTest
+class CustomerRepositoryTest {
+    @Autowired
+    CustomerRepository customerRepo;
+
+    @Test
+    void TestCreateCustomer() {
+        Customer savedCustomer = customerRepo.save(Customer.builder().name("New Customer").build());
+
+        assertThat(savedCustomer).isNotNull();
+
+        UUID id = savedCustomer.getId();
+        log.info("the generated UUID {}",id);
+        assertThat(id).isNotNull();
+    }
+
+
+}
