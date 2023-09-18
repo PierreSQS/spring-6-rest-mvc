27d0e9b0a2b429fe6ae0aeb30f7b77b5e58ea7f8
PierreSQS <nomo_messina@web.de>
2023-09-18 15:54:38 +0200

    Sec9_Assn17: implemented missing Customer Operations

--- src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
+++ src/main/java/guru/springframework/spring6restmvc/services/CustomerServiceJPA.java
@@ -1,11 +1,14 @@
 package guru.springframework.spring6restmvc.services;
 
+import guru.springframework.spring6restmvc.controller.NotFoundException;
+import guru.springframework.spring6restmvc.entities.Customer;
 import guru.springframework.spring6restmvc.mappers.CustomerMapper;
 import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -34,21 +37,46 @@ public class CustomerServiceJPA implements CustomerService {
 
     @Override
     public CustomerDTO saveNewCustomer(CustomerDTO customerDTO) {
-        return null;
+        Customer customerToSave = customerMapper.customerDtoToCustomer(customerDTO);
+        return customerMapper.customerToCustomerDto(customerRepo.save(customerToSave));
     }
 
     @Override
     public void updateCustomerById(UUID customerId, CustomerDTO customerDTO) {
+        Optional<Customer> optFoundCustomerById = customerRepo.findById(customerId);
 
+        if (optFoundCustomerById.isPresent()) {
+            Customer customerToUpdate = optFoundCustomerById.get();
+            customerToUpdate.setName(customerDTO.getName());
+            customerToUpdate.setCreatedDate(customerDTO.getCreatedDate());
+            customerToUpdate.setUpdateDate(customerDTO.getUpdateDate());
+            customerRepo.save(customerToUpdate);
+
+        } else {
+            throw new NotFoundException("Customer Not Found");
+        }
     }
 
     @Override
     public void deleteCustomerById(UUID customerId) {
-
+        Customer customerToDelete = customerRepo.findById(customerId)
+                .orElseThrow(() -> new NotFoundException("Customer Not Found!!"));
+        customerRepo.deleteById(customerToDelete.getId());
     }
 
     @Override
     public void patchCustomerById(UUID customerId, CustomerDTO customerDTO) {
+
+        Optional<Customer> optFoundCustByID = customerRepo.findById(customerId);
+        if (optFoundCustByID.isPresent()) {
+            if (StringUtils.hasText(optFoundCustByID.get().getName())) {
+                optFoundCustByID.get().setName(customerDTO.getName());
+
+                customerRepo.save(optFoundCustByID.get());
+            }
+        } else {
+            throw new NotFoundException("Customer Not Found");
+        }
 
     }
 }
--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -1,18 +1,25 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.entities.Customer;
+import guru.springframework.spring6restmvc.mappers.CustomerMapper;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class CustomerControllerIT {
@@ -22,25 +29,98 @@ class CustomerControllerIT {
 
     @Autowired
     CustomerRepository customerRepo;
+    @Autowired
+    private CustomerMapper customerMapper;
 
     @Test
     void patchCustomerById() {
-        fail("Not Implemented yet!!");
+        List<CustomerDTO> customerDTOList = customerRepo.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .toList();
+
+        CustomerDTO customerToPatch = CustomerDTO.builder().name("Customer to Patch").build();
+
+        // Patch the first customer
+        ResponseEntity<Void> respEntity = customerController.patchCustomerById(customerDTOList.get(0).getId(), customerToPatch);
+
+        assertThat(respEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+        Customer updatedCustomer = customerRepo.findAll().get(0);
+        assertThat(updatedCustomer.getName()).isEqualTo("Customer to Patch");
     }
 
     @Test
-    void deleteCustomerById() {
-        fail("Not Implemented yet!!");
+    void deleteCustomerNotFound() {
+        UUID customerID = UUID.randomUUID();
+
+        // AssertJ assertion
+        assertThatThrownBy(() -> customerController.deleteCustomerById(customerID))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("Customer Not Found!!");
+
+        // Junit5 assertion
+        assertThrows(NotFoundException.class,() -> customerController.deleteCustomerById(customerID));
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void deleteCustomerExistingCustomerById() {
+        List<CustomerDTO> customerDTOList = customerRepo.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDto)
+                .toList();
+
+        // Delete first customer in the list
+        ResponseEntity<Void> respEntity = customerController.deleteCustomerById(customerDTOList.get(0).getId());
+
+        assertThat(respEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+        assertThat(customerRepo.count()).isEqualTo(2);
     }
 
     @Test
-    void updateCustomerByID() {
-        fail("Not Implemented yet!!");
+    void updateCustomerNotFound() {
+        UUID customerID = UUID.randomUUID();
+        CustomerDTO updatedCustomerDTO = CustomerDTO.builder().name("Updated Customer").build();
+
+        // AssertJ Assertion
+        assertThatThrownBy(() -> customerController.updateCustomerByID(customerID, updatedCustomerDTO))
+                .isInstanceOf(NotFoundException.class)
+                .hasMessage("Customer Not Found");
+
+        // Junit 5 Assertion
+        assertThrows(NotFoundException.class,
+                () -> customerController.updateCustomerByID(customerID, updatedCustomerDTO));
+
     }
 
+    @Test
+    void updateExistingCustomerByID() {
+        UUID customerID = customerRepo.findAll().get(0).getId();
+
+        ResponseEntity<Void> respEntity = customerController.updateCustomerByID(customerID,
+                CustomerDTO.builder().name("Updated Customer").build());
+
+        assertThat(respEntity.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        Customer updatedCustomer = customerRepo.findById(customerID).get();
+        assertThat(updatedCustomer.getName()).isEqualTo("Updated Customer");
+
+    }
+
+    @Rollback
+    @Transactional
     @Test
     void saveNewCustomer() {
-        fail("Not Implemented yet!!");
+        CustomerDTO newCustomerDTO = CustomerDTO.builder().name("New Customer").build();
+        ResponseEntity<HttpHeaders> respEntity = customerController.saveNewCustomer(newCustomerDTO);
+
+        String[] pathFragments= respEntity.getHeaders().getLocation().getPath().split("/");
+
+        assertThat(pathFragments[4]).isNotNull();
+
+        assertThat(respEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(customerRepo.count()).isEqualTo(4);
     }
 
     @Rollback
