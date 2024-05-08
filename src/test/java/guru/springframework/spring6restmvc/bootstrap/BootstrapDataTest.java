66e7ea2ba6e2a233dd1a24ebf2a126cd81b05923
PierreSQS <nomo_messina@web.de>
2024-05-08 09:16:03 +0200

    Sec15_Chap160: fixed the issue while running the app from a jar

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ResourceLoader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,16 +26,19 @@ class BootstrapDataTest {
     @Autowired
     BeerCsvService csvService;
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
     BootstrapData bootstrapData;
 
     @BeforeEach
     void setUp() {
-        bootstrapData = new BootstrapData(beerRepository, customerRepository, csvService);
+        bootstrapData = new BootstrapData(beerRepository, customerRepository, csvService,resourceLoader);
     }
 
     @Test
     void Testrun() throws Exception {
-        bootstrapData.run(null);
+        bootstrapData.run();
 
         assertThat(beerRepository.count()).isEqualTo(2413);
         assertThat(customerRepository.count()).isEqualTo(3);
