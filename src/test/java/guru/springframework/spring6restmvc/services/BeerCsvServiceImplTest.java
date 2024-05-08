6fd0cad1b55d07670c1247f20c5cfc2c1299e150
PierreSQS <nomo_messina@web.de>
2024-05-08 11:11:45 +0200

    Sec16_Assn20: fixed the issue when loading the CSV File from the jar

--- src/test/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImplTest.java
+++ src/test/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImplTest.java
@@ -1,15 +1,18 @@
 package guru.springframework.spring6restmvc.services;
 
 import guru.springframework.spring6restmvc.model.BeerCSVRecord;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class BeerCsvServiceImplTest {
 
     BeerCsvService beerCsvService = new BeerCsvServiceImpl();
@@ -19,10 +22,10 @@ class BeerCsvServiceImplTest {
 
         File file = ResourceUtils.getFile("classpath:csvdata/beers.csv");
 
-        List<BeerCSVRecord> recs = beerCsvService.convertCSV(file);
+        List<BeerCSVRecord> recs = beerCsvService.convertCSV(new FileReader(file));
 
-        System.out.println(recs.size());
+        log.info("BeerCSVRecords amount: {}",recs.size());
 
-        assertThat(recs.size()).isGreaterThan(0);
+        assertThat(recs.size()).isPositive();
     }
 }
