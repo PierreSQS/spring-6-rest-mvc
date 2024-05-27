2dbb6a938d8c76301f0e2adce8ac812f6c7da48e
PierreSQS <nomo_messina@web.de>
2024-05-27 02:49:08 +0200

    Sec15_Chap160: alternative to commit 71203edb

--- src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
+++ src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -2,8 +2,13 @@ package guru.springframework.spring6restmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+// Fixes the PageImpl Serialization issue
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Spring6RestMvcApplication {
 
     public static void main(String[] args) {
