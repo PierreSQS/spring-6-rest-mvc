de8235c37c407ca900484bf6663acd0de5292e15
PierreSQS <nomo_messina@web.de>
2024-05-30 10:09:27 +0200

    Sec16_Chap170: adapted new Entity

---
+++ src/main/java/guru/springframework/spring6restmvc/entities/BeerOrderShipment.java
@@ -0,0 +1,51 @@
+package guru.springframework.spring6restmvc.entities;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/**
+ * Modified by Pierrot, 30.05.2024
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class BeerOrderShipment {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false )
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    private String trackingNumber;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedDate;
+
+}
