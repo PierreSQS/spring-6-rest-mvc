d751905ae0cec00667e8f8a7065ca822b8ee75e0
PierreSQS <nomo_messina@web.de>
2024-06-15 09:33:43 +0200

    Sec21_Chap215-XXX: code for the issue on the csv-template

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerTest.java
@@ -69,7 +69,8 @@ class BeerControllerTest {
         verify(beerService).patchBeerById(uuidArgumentCaptor.capture(), beerArgumentCaptor.capture());
 
         assertThat(beer.getId()).isEqualTo(uuidArgumentCaptor.getValue());
-        assertThat(beerMap.get("beerName")).isEqualTo(beerArgumentCaptor.getValue().getBeerName());
+
+        assertThat(beerMap).containsEntry("beerName" , beerArgumentCaptor.getValue().getBeerName());
     }
 
     @Test
