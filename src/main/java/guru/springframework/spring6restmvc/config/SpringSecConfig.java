ea8b26b4fffb099ff13bb98ce27933cd0f0bfd3e
PierreSQS <nomo_messina@web.de>
2024-06-17 14:12:27 +0200

    Sec21_Chap220-XXX: added httpBasic Authentication

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -12,7 +13,7 @@ public class SpringSecConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(csrf -> csrf
                 .ignoringRequestMatchers("/api/**")
-        );
+        ).httpBasic(Customizer.withDefaults());
 
         return httpSecurity.build();
     }
