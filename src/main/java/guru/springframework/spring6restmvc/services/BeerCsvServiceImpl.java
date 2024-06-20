5eac181269911811a567e17c9811310c1b8b65b8
PierreSQS <nomo_messina@web.de>
2024-06-20 02:38:34 +0200

    Sec39_Chap359-XXX: fixed the issue on the csv-template

--- src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -4,25 +4,19 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import guru.springframework.spring6restmvc.model.BeerCSVRecord;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.Reader;
 import java.util.List;
 
 /**
- * Created by jt, Spring Framework Guru.
+ * Modified by Pierrot on 20-06-2024.
  */
 @Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
-    public List<BeerCSVRecord> convertCSV(File csvFile) {
+    public List<BeerCSVRecord> convertCSV(Reader reader) {
 
-        try {
-            return new CsvToBeanBuilder<BeerCSVRecord>(new FileReader(csvFile))
-                    .withType(BeerCSVRecord.class)
-                    .build().parse();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        return new CsvToBeanBuilder<BeerCSVRecord>(reader)
+                .withType(BeerCSVRecord.class)
+                .build().parse();
     }
 }
