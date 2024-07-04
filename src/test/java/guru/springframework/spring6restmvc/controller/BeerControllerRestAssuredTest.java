799b983068d785275578e4982f368eaca26fa736
PierreSQS <nomo_messina@web.de>
2024-07-04 14:03:03 +0200

    Sec40_Chap363-NOK: added Security Config

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -6,6 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static io.restassured.RestAssured.given;
 
@@ -13,7 +19,23 @@ import static io.restassured.RestAssured.given;
  * Created by Pierrot on 04-07-2024.
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(value = {BeerControllerRestAssuredTest.TestSecurityConfig.class})
+@ComponentScan(basePackages = {"guru.springframework.spring6restmvc"})
 class BeerControllerRestAssuredTest {
+
+    @Configuration
+    public static class TestSecurityConfig {
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.authorizeHttpRequests(authorize -> authorize
+                    .requestMatchers("/api/v1/beer").permitAll()
+                    .anyRequest().authenticated());
+
+            return httpSecurity.build();
+        }
+
+    }
 
     @LocalServerPort
     Integer localPort;
