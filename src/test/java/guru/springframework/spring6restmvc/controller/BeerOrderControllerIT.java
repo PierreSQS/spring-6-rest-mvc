5800194581af91f7614ca708baaa29702a2f0b21
PierreSQS <nomo_messina@web.de>
2024-09-23 06:33:56 +0200

    Sec47_Chap428: added a TDD Test for Update Operation

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerIT.java
@@ -6,9 +6,12 @@ import guru.springframework.spring6restmvc.entities.BeerOrder;
 import guru.springframework.spring6restmvc.entities.Customer;
 import guru.springframework.spring6restmvc.model.BeerOrderCreateDTO;
 import guru.springframework.spring6restmvc.model.BeerOrderLineCreateDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderLineUpdateDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderUpdateDTO;
 import guru.springframework.spring6restmvc.repositories.BeerOrderRepository;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import static guru.springframework.spring6restmvc.controller.BeerOrderController.BEER_ORDER_PATH;
@@ -25,6 +29,7 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -50,11 +55,44 @@ class BeerOrderControllerIT {
     ObjectMapper objectMapper;
 
     @Test
+    void updateBeerOrder() throws Exception {
+        // find an existing BeerOrder (the first one in this case)
+        BeerOrder firstFoundBeerOrder = beerOrderRepo.findAll().getFirst();
+
+        // Set of updates of BeerOrder lines DTOs
+        Set<BeerOrderLineUpdateDTO> updateBeerOrderLinesDTOs = new HashSet<>();
+
+        firstFoundBeerOrder.getBeerOrderLines().forEach(
+                beerOrderLine -> updateBeerOrderLinesDTOs.add(BeerOrderLineUpdateDTO.builder()
+                        .id(beerOrderLine.getId())
+                        .beerID(beerOrderLine.getBeer().getId())
+                        .orderQuantity(beerOrderLine.getOrderQuantity())
+                        .quantityAllocated(beerOrderLine.getQuantityAllocated())
+                        .build())
+        );
+
+        val beerOrderUpdateDTO = BeerOrderUpdateDTO.builder()
+                .customerID(firstFoundBeerOrder.getId())
+                .customerRef("Test Customer Ref")
+                .beerOrderLineUpdateDTOs(updateBeerOrderLinesDTOs)
+                .build();
+
+        mockMvc.perform(put(BEER_ORDER_PATH_ID,firstFoundBeerOrder.getId())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(beerOrderUpdateDTO))
+                        .with(BeerControllerTest.jwtRequestPostProcessor))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+
+    }
+
+    @Test
     void createBeerOrder() throws Exception {
         // find an existing customer and set an order and orderlines on it
         Customer firstFoundCustomer = customerRepo.findAll().getFirst();
 
-        // find an existing beer and set it as reference in the beer orderline
+        // find an existing beer and set it as reference in the BeerOrder line
         Beer firstFoundBeer = beerRepo.findAll().getFirst();
 
         BeerOrderCreateDTO beerOrderCreateDTO = BeerOrderCreateDTO.builder()
@@ -65,7 +103,8 @@ class BeerOrderControllerIT {
                                 .beerID(firstFoundBeer.getId())
                                 .orderQuantity(1)
                                 .build()
-                )).build();
+                ))
+                .build();
 
         mockMvc.perform(post(BEER_ORDER_PATH)
                         .contentType(MediaType.APPLICATION_JSON)
