0b8712abf8bf87a675605093ced0a0b97c195dbd
PierreSQS <nomo_messina@web.de>
2024-09-24 07:48:32 +0200

    Sec47_Chap429-NOK: fixed the previous issue (4118f705)

--- src/main/java/guru/springframework/spring6restmvc/model/BeerOrderShipmentUpdateDTO.java
+++ src/main/java/guru/springframework/spring6restmvc/model/BeerOrderShipmentUpdateDTO.java
@@ -1,14 +1,18 @@
 package guru.springframework.spring6restmvc.model;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by Pierrot on 21-09-2024.
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BeerOrderShipmentUpdateDTO {
 
     @NotBlank
