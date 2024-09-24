4118f7053707c35e3a308fb332fb80aa6f9bb345
PierreSQS <nomo_messina@web.de>
2024-09-24 07:32:37 +0200

    Sec47_Chap429-NOK: fixed the previous issue (80f5a4dc)

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerOrderControllerIT.java
@@ -19,6 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -55,6 +56,7 @@ class BeerOrderControllerIT {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Transactional
     @Test
     void updateBeerOrder() throws Exception {
         // find an existing BeerOrder (the first one in this case)
