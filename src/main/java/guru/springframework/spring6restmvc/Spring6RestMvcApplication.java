5b0f2bacc4f9c06d0c88e41464350c9d88c3eecc
PierreSQS <nomo_messina@web.de>
2024-10-03 14:24:47 +0200

    Sec47_Chap432-XXX: to fix failing test because serialization mode added

--- src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
+++ src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -9,7 +9,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 
 @EnableCaching
 @SpringBootApplication
-//@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class Spring6RestMvcApplication {
 
     public static void main(String[] args) {
