f64269557f8f0c2830bf31fdc235816b23a5b659
PierreSQS <nomo_messina@web.de>
2024-10-04 16:42:10 +0200

    Sec40_Chap363: deactivated Spring Security Config

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerRestAssuredTest.java
@@ -6,14 +6,35 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ActiveProfiles;
 
 import static io.restassured.RestAssured.given;
 
 /**
  * Created by Pierrot on 04-10-2024.
  */
+@ActiveProfiles("test") // to overcome the Spring Securtity config (See the SpringSecConfig class !!!
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import({BeerControllerRestAssuredTest.TestConfig.class})
+@ComponentScan(basePackages = "guru.springframework.spring6restmvc")
 class BeerControllerRestAssuredTest {
+
+    @Configuration
+    public static class TestConfig {
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+
+            return httpSecurity.build();
+        }
+    }
 
     @LocalServerPort
     Integer localPort;
