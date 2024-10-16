b0375b0350cddc74875b3b5a8bbd9ceac4b01279
PierreSQS <nomo_messina@web.de>
2024-10-16 18:15:35 +0200

    Sec46_Chap409-XXX: fixed the reading of the CSV file issue

--- src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerCsvServiceImpl.java
@@ -4,25 +4,18 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import guru.springframework.spring6restmvc.model.BeerCSVRecord;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.Reader;
 import java.util.List;
-
 /**
  *  * Modified by Pierrot on 2024-10-16.
  */
 @Service
 public class BeerCsvServiceImpl implements BeerCsvService {
     @Override
-    public List<BeerCSVRecord> convertCSV(File csvFile) {
+    public List<BeerCSVRecord> convertCSV(Reader reader) {
 
-        try {
-            return new CsvToBeanBuilder<BeerCSVRecord>(new FileReader(csvFile))
-                    .withType(BeerCSVRecord.class)
-                    .build().parse();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        return new CsvToBeanBuilder<BeerCSVRecord>(reader)
+                .withType(BeerCSVRecord.class)
+                .build().parse();
     }
 }
