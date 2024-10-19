e60a5e72cb11fa95839e0af6d1492f07a4b60283
PierreSQS <nomo_messina@web.de>
2024-10-19 16:35:14 +0200

    Sec46_Chap413: fixed the asynchronous issue

--- src/main/java/guru/springframework/spring6restmvc/listeners/BeerCreatedListener.java
+++ src/main/java/guru/springframework/spring6restmvc/listeners/BeerCreatedListener.java
@@ -3,6 +3,7 @@ package guru.springframework.spring6restmvc.listeners;
 import guru.springframework.spring6restmvc.events.BeerCreatedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class BeerCreatedListener {
 
+    @Async // Marks the method as candidate for asynchronous execution
     @EventListener
     public void listen(BeerCreatedEvent beerCreatedEvent) {
         log.info("I hear a beer was created!");
