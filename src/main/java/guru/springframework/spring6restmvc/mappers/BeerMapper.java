0253b167423d7d287a99205de0a21a11b979008e
PierreSQS <nomo_messina@web.de>
2024-10-20 16:31:59 +0200

    Sec46_Chap415: added Audit Mapper

--- src/main/java/guru/springframework/spring6restmvc/mappers/BeerMapper.java
+++ src/main/java/guru/springframework/spring6restmvc/mappers/BeerMapper.java
@@ -1,17 +1,27 @@
 package guru.springframework.spring6restmvc.mappers;
 
 import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.BeerAudit;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
- * Created by jt, Spring Framework Guru.
+ * Modified by Pierrot on 2024-10-20.
  */
 @Mapper
 public interface BeerMapper {
 
+    @Mapping(target = "categories", ignore = true)
+    @Mapping(target = "beerOrderLines", ignore = true)
     Beer beerDtoToBeer(BeerDTO dto);
 
     BeerDTO beerToBeerDto(Beer beer);
+
+    @Mapping(target = "createdDateAudit", ignore = true)
+    @Mapping(target = "auditID", ignore = true)
+    @Mapping(target = "auditEventType", ignore = true)
+    @Mapping(target = "principalName", ignore = true)
+    BeerAudit beerToBeerAudit(Beer beer);
 
 }
