2a56980c09d3e54eb79df14a8cd1eb65519a98e6
PierreSQS <nomo_messina@web.de>
2024-10-29 20:50:25 +0100

    Sec46_ASSN44-XXX: adapted property files to assignment settings

--- src/scripts/pierrot-mysql-init.sql
+++ src/scripts/pierrot-mysql-init.sql
@@ -1,6 +1,7 @@
-DROP DATABASE IF EXISTS jt_spring6_sect16_chap170_db;
+DROP DATABASE IF EXISTS jt_spring6_sect46_assn44_db;
 DROP USER IF EXISTS `restadmin`@`%`;
-CREATE DATABASE IF NOT EXISTS jt_spring6_sect16_chap170_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS jt_spring6_sect46_assn44_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE jt_spring6_sect46_assn44_db;
 CREATE USER IF NOT EXISTS `restadmin`@`%` IDENTIFIED WITH mysql_native_password BY 'restadmin';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
-CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `jt_spring6_sect16_chap170_db`.* TO `restadmin`@`%`;
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `jt_spring6_sect46_assn44_db`.* TO `restadmin`@`%`;
