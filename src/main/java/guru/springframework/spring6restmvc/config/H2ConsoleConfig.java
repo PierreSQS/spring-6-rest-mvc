ac3d27854cba3467bbf205bb588c784bc1ed7540
PierreSQS <nomo_messina@web.de>
2024-10-31 00:15:58 +0100

    Sec46_ASSN44-XXX-NOK: fixed the issue with H2 Console Bean

---
+++ src/main/java/guru/springframework/spring6restmvc/config/H2ConsoleConfig.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6restmvc.config;
+
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2ConsoleConfig {
+    @Bean
+    public H2ConsoleProperties h2ConsoleProperties() {
+        return new H2ConsoleProperties();
+    }
+}
