ac83bf0ab6444b3b9eddaf04253f4bdff61c5a3a
PierreSQS <nomo_messina@web.de>
2024-10-31 09:13:04 +0100

    Sec46_ASSN44-XXX: fixed the previous issue (ac3d2785)

--- src/main/java/guru/springframework/spring6restmvc/config/H2ConsoleConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/H2ConsoleConfig.java
@@ -3,10 +3,12 @@ package guru.springframework.spring6restmvc.config;
 import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class H2ConsoleConfig {
     @Bean
+    @Primary
     public H2ConsoleProperties h2ConsoleProperties() {
         return new H2ConsoleProperties();
     }
