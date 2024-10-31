cbb816a8360f0fe60b661ab33bac4374fec4d305
PierreSQS <nomo_messina@web.de>
2024-10-31 11:57:06 +0100

    Sec46_ASSN44-XXX-NOK: optimized Security Config

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,11 +13,27 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- *  * Modified by Pierrot on 2024-10-16.
+ *  * Modified by Pierrot on 2024-10-31.
  */
 @Profile("!test")
-@Configuration
+@Configuration(proxyBeanMethods = false) // ACCORDING TO SPRING BOOT DOCUMENTATION
 public class SpringSecConfig {
+
+    // ACCORDING TO SPRING BOOT DOCUMENTATION
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
+        // ALLOW ALL REQUESTS TO THE H2 CONSOLE
+        http.securityMatcher(PathRequest.toH2Console());
+
+        // DISABLE CSRF FOR H2 CONSOLE
+        http.csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers("/h2-console/**"));
+
+        // ALLOW FRAMES FROM SAME ORIGIN
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        return http.build();
+    }
+
 
     @Bean
     @Order(1)
@@ -31,19 +48,14 @@ public class SpringSecConfig {
     @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize
-                        // ALLOW ALL REQUESTS TO THE H2 CONSOLE
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
                         .requestMatchers("/v3/api-docs**",
                                                   "/swagger-ui/**",
                                                   "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated())
-                // DISABLE CSRF FOR H2 CONSOLE
-                .csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers("/h2-console/**"))
                 .oauth2ResourceServer(httpSecurityOAuth2ResourceServerConfigurer ->
                        httpSecurityOAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults()));
 
-        // ALLOW FRAMES FROM SAME ORIGIN
-        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
 
         return http.build();
     }
