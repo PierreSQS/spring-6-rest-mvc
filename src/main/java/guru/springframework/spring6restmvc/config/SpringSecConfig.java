744f4d3f2c28b5e570394ffbabaf403edb971ff5
PierreSQS <nomo_messina@web.de>
2024-11-01 07:39:57 +0100

    Sec45_Chap403-XXX: enabled H2-console access

--- src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/spring6restmvc/config/SpringSecConfig.java
@@ -1,12 +1,14 @@
 package guru.springframework.spring6restmvc.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -28,8 +30,19 @@ public class SpringSecConfig {
     @Bean
     @Order(2)
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize ->
-                        authorize.requestMatchers(
+        // ALLOW H2 CONSOLE ACCESS
+        http.authorizeHttpRequests(authorize -> authorize.
+                requestMatchers(PathRequest.toH2Console()).permitAll());
+
+        // DISABLE CSRF PROTECTION ON H2-CONSOLE PATH
+        http.csrf(csrfConfigurer -> csrfConfigurer.ignoringRequestMatchers(PathRequest.toH2Console()));
+
+        // SET THE HEADER X-FRAME-OPTIONS TO SAMEORIGIN ON RESPONSES FROM THE CONSOLE
+        // e.g. ALLOW X-FRAMES ON SAME ORIGIN
+        http.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+
+        http.authorizeHttpRequests(authorize -> authorize.
+                        requestMatchers(
                                 "/v3/api-docs**",
                                         "/swagger-ui/**",
                                         "/swagger-ui.html")
