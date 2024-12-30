68f47112148eeaa5dcb6117a9249bbfaf30a1624
PierreSQS <nomo_messina@web.de>
2024-12-30 19:43:36 +0100

    Sec9_ASSN15: refined BootStrap Test - final solution

--- src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring6restmvc/bootstrap/BootstrapDataTest.java
@@ -1,15 +1,11 @@
 package guru.springframework.spring6restmvc.bootstrap;
 
-import guru.springframework.spring6restmvc.entities.Beer;
-import guru.springframework.spring6restmvc.entities.Customer;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,15 +21,11 @@ class BootstrapDataTest {
 
     @Test
     void testLoadBeerData() {
-        List<Beer> allBeers = beerRepo.findAll();
-
-        assertThat(allBeers.size()).isNotZero();
+        assertThat(beerRepo.count()).isEqualTo(3);
     }
 
     @Test
     void testLoadCustomerData() {
-        List<Customer> allCustomers = customerRepo.findAll();
-
-        assertThat(allCustomers.size()).isNotZero();
+        assertThat(customerRepo.count()).isEqualTo(3);
     }
 }
