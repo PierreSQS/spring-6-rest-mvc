b858db369f3f3bd576149ccfc2707c2b4304b5ef
PierreSQS <nomo_messina@web.de>
2024-12-31 01:12:46 +0100

    Sec9_Chap103: testing for expected exceptions

--- src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerDTO;
 import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
@@ -9,8 +10,10 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 class BeerControllerIT {
@@ -20,6 +23,26 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerByIdNotFound() {
+        UUID uuid = UUID.randomUUID();
+        assertThatThrownBy(() -> beerController.getBeerById(uuid))
+                .isInstanceOf(NotFoundException.class);
+
+    }
+
+    @Test
+    void testGetBeerByID() {
+        // find first Beer in the DB
+        Beer beerFound = beerRepository.findAll().getLast();
+
+        // find it from the Controller and then Assert
+        BeerDTO foundBeerDTO = beerController.getBeerById(beerFound.getId());
+
+        assertThat(foundBeerDTO.getId()).isEqualTo(beerFound.getId());
+
+    }
 
     @Test
     void testListBeers() {
