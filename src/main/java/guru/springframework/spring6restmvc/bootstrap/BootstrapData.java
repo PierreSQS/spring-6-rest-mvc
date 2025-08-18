e347c1f15216058002a24e7a901d29d223d5a29a
PierreSQS <nomo_messina@web.de>
2025-08-18 17:55:54 +0200

    Sec53_Chap493-XXX: enhanced logging and streamline beer/customer data loading

--- src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6restmvc/bootstrap/BootstrapData.java
@@ -11,6 +11,7 @@ import guru.springframework.spring6restmvc.repositories.BeerRepository;
 import guru.springframework.spring6restmvc.repositories.CustomerRepository;
 import guru.springframework.spring6restmvc.services.BeerCsvService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.CommandLineRunner;
@@ -22,13 +23,13 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 /**
- * Created by jt, Spring Framework Guru.
+ * Modified by Pierrot on 2025-08-18.
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BootstrapData implements CommandLineRunner {
@@ -75,6 +76,7 @@ public class BootstrapData implements CommandLineRunner {
                                         .beer(beerIterator.next())
                                         .orderQuantity(1)
                                         .build(),
+
                                 BeerOrderLine.builder()
                                         .beer(beerIterator.next())
                                         .orderQuantity(2)
@@ -82,7 +84,8 @@ public class BootstrapData implements CommandLineRunner {
                         )).build());
             });
 
-            val orders = beerOrderRepository.findAll();
+            int totalCustomer = customers.size();
+            log.info("loaded {} BeerOrders to {} Customers ", beerOrderRepository.count(),totalCustomer);
         }
 
 
@@ -106,6 +109,7 @@ public class BootstrapData implements CommandLineRunner {
                     case "Saison / Farmhouse Ale" -> BeerStyle.SAISON;
                     case "Fruit / Vegetable Beer", "Winter Warmer", "Berliner Weissbier" -> BeerStyle.WHEAT;
                     case "English Pale Ale" -> BeerStyle.PALE_ALE;
+                    case "Gose" -> BeerStyle.GOSE;
                     default -> BeerStyle.PILSNER;
                 };
 
@@ -117,6 +121,9 @@ public class BootstrapData implements CommandLineRunner {
                                 .quantityOnHand(beerCSVRecord.getCount())
                         .build());
             });
+
+            log.info("loaded {} Beers from CSV-File ", recs.size());
+            log.info("total Beers loaded: {}", beerRepository.count());
         }
     }
 
@@ -152,9 +159,10 @@ public class BootstrapData implements CommandLineRunner {
                     .updateDate(LocalDateTime.now())
                     .build();
 
-            beerRepository.save(beer1);
-            beerRepository.save(beer2);
-            beerRepository.save(beer3);
+            List<Beer> additionalBeers = List.of(beer1, beer2, beer3);
+            beerRepository.saveAll(additionalBeers);
+
+            log.info("loaded {} Beers manually", additionalBeers.size());
         }
 
     }
@@ -183,7 +191,10 @@ public class BootstrapData implements CommandLineRunner {
                     .updateDate(LocalDateTime.now())
                     .build();
 
-            customerRepository.saveAll(Arrays.asList(customer1, customer2, customer3));
+            List<Customer> customers = List.of(customer1, customer2, customer3);
+            customerRepository.saveAll(customers);
+
+            log.info("loaded {} Customers",customers.size());
         }
 
     }
