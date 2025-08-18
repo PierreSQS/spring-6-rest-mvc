f7978cfc690c2e8cd88cdfe7ea93a6401b3aa746
PierreSQS <nomo_messina@web.de>
2025-08-18 17:59:33 +0200

    Sec53_Chap493-XXX: added Pierrot schema script

---
+++ src/scripts/pierrot-mysql-init.sql
@@ -0,0 +1,7 @@
+DROP DATABASE IF EXISTS jt_spring6_sect47_db;
+DROP USER IF EXISTS `restadmin`@`%`;
+CREATE DATABASE IF NOT EXISTS jt_spring6_sect47_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS `restadmin`@`%` IDENTIFIED WITH mysql_native_password BY 'restadmin';
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `jt_spring6_sect47_db`.* TO `restadmin`@`%`;
+USE jt_spring6_sect47_db;
