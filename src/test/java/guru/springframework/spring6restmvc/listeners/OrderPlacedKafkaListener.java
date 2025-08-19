ec10183cb4c221e25f8e8e3c991178bd59c1167b
PierreSQS <nomo_messina@web.de>
2025-08-20 01:55:38 +0200

    Sec53_Chap496: fixed the previous issue (836c3628)

--- src/test/java/guru/springframework/spring6restmvc/listeners/OrderPlacedKafkaListener.java
+++ src/test/java/guru/springframework/spring6restmvc/listeners/OrderPlacedKafkaListener.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring6restmvc.listeners;
 
 import guru.springframework.spring6restmvc.config.KafkaConfig;
+import guru.springframework.spring6restmvcapi.events.OrderPlacedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -14,7 +15,7 @@ public class OrderPlacedKafkaListener {
     AtomicInteger msgCounter = new AtomicInteger(0);
 
     @KafkaListener(topics = KafkaConfig.ORDER_PLACED_TOPIC, groupId = "KafkaIntegrationTestGroup")
-    public void onReceive(String message) {
+    public void onReceive(OrderPlacedEvent message) {
         // Process the received message
         log.info("Received message: {}", message);
         msgCounter.incrementAndGet();
