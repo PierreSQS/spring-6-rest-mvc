836c3628b0d21fc5fef8d8639ec66a776bc1fc18
PierreSQS <nomo_messina@web.de>
2025-08-20 01:43:14 +0200

    Sec53_Chap496-NOK: step2 - testing with Embedded Kafka Broker

--- src/test/java/guru/springframework/spring6restmvc/listeners/OrderPlacedKafkaListener.java
+++ src/test/java/guru/springframework/spring6restmvc/listeners/OrderPlacedKafkaListener.java
@@ -1,4 +1,22 @@
 package guru.springframework.spring6restmvc.listeners;
 
+import guru.springframework.spring6restmvc.config.KafkaConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Slf4j
+@Component
 public class OrderPlacedKafkaListener {
+
+    AtomicInteger msgCounter = new AtomicInteger(0);
+
+    @KafkaListener(topics = KafkaConfig.ORDER_PLACED_TOPIC, groupId = "KafkaIntegrationTestGroup")
+    public void onReceive(String message) {
+        // Process the received message
+        log.info("Received message: {}", message);
+        msgCounter.incrementAndGet();
+    }
 }
--- src/test/java/guru/springframework/spring6restmvc/listeners/OrderPlacedListenerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/listeners/OrderPlacedListenerTest.java
@@ -1,17 +1,69 @@
 package guru.springframework.spring6restmvc.listeners;
 
+import guru.springframework.spring6restmvcapi.events.OrderPlacedEvent;
+import guru.springframework.spring6restmvcapi.model.BeerOrderDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.kafka.test.utils.ContainerTestUtils;
+import org.springframework.test.annotation.DirtiesContext;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@EmbeddedKafka(kraft = true, partitions = 1, controlledShutdown = true)
 class OrderPlacedListenerTest {
+
+    // Registry for managing Kafka listeners
+    @Autowired
+    KafkaListenerEndpointRegistry kafkaListenerEndpointRegistry;
+
+    // Event listener to be tested
+    @Autowired
+    OrderPlacedListener orderPlacedListener;
+
+    // Kafka listener to be tested
+    @Autowired
+    OrderPlacedKafkaListener orderPlacedKafkaListener;
 
     @BeforeEach
     void setUp() {
+        // Ensure the partition is assigned to the listener before each test
+        kafkaListenerEndpointRegistry.getAllListenerContainers().forEach(
+            container -> ContainerTestUtils.waitForAssignment(container, 1)
+        );
+
     }
 
     @Test
     void listen() {
+
+        OrderPlacedEvent orderPlacedEvent = OrderPlacedEvent.builder()
+                .beerOrderDTO(BeerOrderDTO.builder()
+                        .id(UUID.randomUUID())
+                        .build())
+                .build();
+
+        // This test will start the listener and wait for messages
+        orderPlacedListener.listen(orderPlacedEvent);
+
+        // Verify that the listener has processed the event
+
+        // Wait at most 5s and poll every 500ms until the assertion is met
+        // This is to ensure that the message has been processed and the counter has been incremented
+        await().atMost(5, TimeUnit.SECONDS).pollInterval(500, TimeUnit.MILLISECONDS)
+                .untilAsserted(() -> {
+                    // Check if the message counter has been incremented
+                    assertThat(orderPlacedKafkaListener.msgCounter.get()).isEqualTo(1);
+                });
+
     }
 }
