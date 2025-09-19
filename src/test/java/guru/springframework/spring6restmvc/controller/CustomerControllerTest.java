70e732ac3f6611fc5287e7d0c783b24c6a0c33e4
John Thompson <john@springframework.guru>
2025-09-19 09:58:55 -0400

    Add `@JsonDeserialize` annotation to Customer class

--- src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerTest.java
@@ -1,6 +1,6 @@
 package guru.springframework.spring6restmvc.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import guru.springframework.spring6restmvc.model.Customer;
 import guru.springframework.spring6restmvc.services.CustomerService;
 import guru.springframework.spring6restmvc.services.CustomerServiceImpl;
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
 
 import static org.hamcrest.core.Is.is;
 import static org.mockito.ArgumentMatchers.any;
