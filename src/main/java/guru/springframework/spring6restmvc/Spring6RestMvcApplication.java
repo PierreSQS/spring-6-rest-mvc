63ad984f15058e2c3656b1589acb908d911ffdc7
PierreSQS <nomo_messina@web.de>
2025-10-22 22:18:45 +0200

    Sec24_Chap250-XXX: fixed the pagination issue

--- src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
+++ src/main/java/guru/springframework/spring6restmvc/Spring6RestMvcApplication.java
@@ -2,8 +2,10 @@ package guru.springframework.spring6restmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class Spring6RestMvcApplication {
 
     public static void main(String[] args) {
