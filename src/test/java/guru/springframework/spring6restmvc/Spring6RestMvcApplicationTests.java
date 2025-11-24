708e1cdaf9ad353c0acd0839a18049781f1aaff6
PierreSQS <nomo_messina@web.de>
2025-11-24 19:23:30 +0100

    Sec9_Chap96-XX: optimized code

--- src/test/java/guru/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
+++ src/test/java/guru/springframework/spring6restmvc/Spring6RestMvcApplicationTests.java
@@ -2,12 +2,18 @@ package guru.springframework.spring6restmvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class Spring6RestMvcApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext appCtx) {
+        assertThat(appCtx).isNotNull();
+
+
     }
 
 }
