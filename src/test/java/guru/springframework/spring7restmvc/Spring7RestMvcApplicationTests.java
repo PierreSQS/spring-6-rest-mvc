3a18075e0835d21f11648107d2ffec972804c4e9
PierreSQS <nomo_messina@web.de>
2025-12-10 16:05:54 +0100

    Sec9_Chap98-XX: renamed package to reflect Spring 7 migration

--- src/test/java/guru/springframework/spring7restmvc/Spring7RestMvcApplicationTests.java
+++ src/test/java/guru/springframework/spring7restmvc/Spring7RestMvcApplicationTests.java
@@ -1,4 +1,4 @@
-package guru.springframework.spring6restmvc;
+package guru.springframework.spring7restmvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-class Spring6RestMvcApplicationTests {
+class Spring7RestMvcApplicationTests {
 
     @Test
     void contextLoads(ApplicationContext appCtx) {
