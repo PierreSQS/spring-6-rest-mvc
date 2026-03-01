3b1c7cee3dc0f3a901f4fbc4c8f71d64384ed0fb
PierreSQS <nomo_messina@web.de>
2026-03-01 16:31:09 +0100

    Sec16_Chap166-XX-NOK: upgraded SB to 4.0.3

--- src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
+++ src/test/java/guru/springframework/spring6restmvc/repositories/CustomerRepositoryTest.java
@@ -3,7 +3,7 @@ package guru.springframework.spring6restmvc.repositories;
 import guru.springframework.spring6restmvc.entities.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
