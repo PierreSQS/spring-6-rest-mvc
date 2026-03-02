8f7374c687575b31e4c4f3ccd9fecbd6edd7787b
PierreSQS <nomo_messina@web.de>
2026-03-02 06:47:50 +0100

    Sec16_Chap167-XX: upgraded code to SB to 4.0.3

--- src/test/java/guru/springframework/spring7restmvc/bootstrap/BootstrapDataTest.java
+++ src/test/java/guru/springframework/spring7restmvc/bootstrap/BootstrapDataTest.java
@@ -6,7 +6,7 @@ import guru.springframework.spring7restmvc.services.BeerCsvService;
 import guru.springframework.spring7restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import static org.assertj.core.api.Assertions.assertThat;
--- src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
+++ src/test/java/guru/springframework/spring7restmvc/controller/BeerControllerIT.java
@@ -8,8 +8,9 @@ import guru.springframework.spring7restmvc.model.BeerStyle;
 import guru.springframework.spring7restmvc.repositories.BeerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
